09f2dcae18708a28245ebbc072c2ba1cf23bcefc
JiaRu <jiaru2014@126.com>
2018-10-06 12:35:25 +0800

    utils.calc_td_ts()

--- bitmex/utils.py
+++ bitmex/utils.py
@@ -35,3 +35,24 @@ def generate_logger(loggername='defaultLogger', loglevel='debug', logfile=None):
     
     return logger
     
+    
+def calculate_td_ts(x, bar_type='1m'):
+    """"2018-09-29T06:00:17.271Z -> 20180929, 617"""
+    # x = '2018-09-29T06:17:34.271Z'
+    # bar_type = '4h'
+    
+    td, sec = x.split('T')
+    td = int(td.replace('-', ''))
+    
+    n, what = int(bar_type[:-1]), bar_type[-1]    
+    h,m,s = sec[:8].split(':')
+    if what == 'h':
+        ts = '%02d' % (int(h)// n * n)
+    elif what == 'm':
+        ts = h + '%02d' % (int(m) // n * n)
+    elif what == 's':
+        ts = h + m + '%02d' % (int(s) // n * n)
+    else:
+        raise ValueError('bar_type pattern should be "\\d[h|m|s]"')
+        
+    return td,int(ts)
