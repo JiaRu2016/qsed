62a2ecd8bf62ad4327ef39c78fed9f64bc9a0fed
JiaRu <jiaru2014@126.com>
2018-10-27 15:06:14 +0800

    [New] query_history_tick/bar()

--- qsed/bitmex/bitmexREST.py
+++ qsed/bitmex/bitmexREST.py
@@ -3,6 +3,7 @@ import datetime
 import json
 import time
 from .APIKeyAuthWithExpires import APIKeyAuthWithExpires
+from .utils import check_bar_type
 from qsUtils import generate_logger
 
 
@@ -17,13 +18,16 @@ class bitmexREST(object):
         self.apiSecret = apiSecret
         self.isTestNet = isTestNet
         
-        self.base_url = 'https://testnet.bitmex.com/api/v1/' if self.isTestNet else 'https://www.bitmex.com/api/v1'
+        self.base_url = 'https://testnet.bitmex.com/api/v1/' if self.isTestNet else 'https://www.bitmex.com/api/v1/'
         self.clientOrderID = 0
         
     def _send_http_request(self, verb, path, postdict=None, query=None):
         """send HTTP request"""
         url = self.base_url + path
-        auth = APIKeyAuthWithExpires(self.apiKey, self.apiSecret)
+        if self.apiKey is not None and self.apiSecret is not None:
+            auth = APIKeyAuthWithExpires(self.apiKey, self.apiSecret)
+        else:
+            auth = None
         return requests.request(verb, url, json=postdict, params=query, auth=auth)
     
     def place_order(self, symbol, side, qty, limit_price, text=''):
@@ -86,13 +90,32 @@ class bitmexREST(object):
     def _add_ts(text):
         return '[API][%s] %s' % (str(datetime.datetime.now()), text)
 
-    def query_history_bars(self):
+    def query_history_bars(self, symbol, startTime, endTime, bar_type):
         """查询历史K线"""
-        pass
+        check_bar_type(bar_type)
 
-    def query_history_ticks(self):
+        verb = 'GET'
+        endpoint = 'trade/bucketed'
+        params = {
+            'symbol': symbol,
+            'startTime': startTime,
+            'endTime': endTime,
+            'binSize': bar_type,
+            'partial': 'false',
+        }
+        return self._page_query(verb, endpoint, params)
+
+    def query_history_ticks(self, symbol, startTime, endTime):
         """查询历史tick"""
-        pass
+
+        verb = 'GET'
+        endpoint = 'trade'
+        params = {
+            'symbol': symbol,
+            'startTime': startTime,
+            'endTime': endTime,
+        }
+        return self._page_query(verb, endpoint, params)
 
     def query_history_execution(self, symbol, startTime=None, endTime=None):
         """查询成交历史"""
@@ -118,6 +141,7 @@ class bitmexREST(object):
         """通用功能：分页查询
         bitmex限制查询每次最多500条，利用params中的 start & count 来分页查询
         """
+        # print('Calling _page_query....')
         result = []
         params['count'] = count
         params['start'] = 0
@@ -130,6 +154,9 @@ class bitmexREST(object):
                     break
                 else:
                     params['start'] += count
+                    # print('new page...')
+            else:
+                self.logger.warning('_page_query() res.ok is not True\nstatus_code:%d\n%s' % (res.status_code, res.content))
         return result
 
 
@@ -177,7 +204,30 @@ def test_query_execution():
     df = pd.DataFrame(result)
     print(df)
 
+
+def test_query_market():
+    """查询历史行情测试"""
+    import pandas as pd
+
+    # from bitmex.bitmexAccountSettings import bitmexAccountSettings
+    # acc = bitmexAccountSettings()
+    # acc.from_config_file('bitmex/BITMEX_connect.json', 'account_real_market_data')
+    #
+    # bm = bitmexREST(apiKey=acc.apiKey, apiSecret=acc.apiSecret, isTestNet=acc.isTestNet)
+    bm = bitmexREST(apiKey=None, apiSecret=None, isTestNet=True)
+
+    print("================ query history ticks ===============")
+    result = bm.query_history_ticks('XBTUSD', '2018-10-27 00:00:00', '2018-10-27 00:01:00')
+    df = pd.DataFrame(result)
+    print(df)
+
+    print("================ query history bars ===============")
+    result_bar = bm.query_history_bars('XBTUSD', '2018-10-27', None, '1h')
+    df_bar = pd.DataFrame(result_bar)
+    print(df_bar)
+
     
 if __name__ == '__main__':
     # test_orders()
-    test_query_execution()
+    # test_query_execution()
+    test_query_market()
